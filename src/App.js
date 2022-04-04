ecae73f5db43dc30e11d2b35903eec914cb6cedc
Folajimmie <hayodele24@gmail.com>
2022-04-04 15:00:45 +0100

    Deploy my gift card task to git pages

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { allGiftCards } from './Data';
+
+function App() {
+  
+  return (
+    <>
+      <nav>
+        <h3 className='header'>Gift-Cards</h3>
+      </nav>
+
+      <div>
+        <div className="cards">
+          {
+            allGiftCards.map( giftcard => {
+              
+              return (
+                <div className="card">
+                  <div className="card-title">
+                    { giftcard.title }
+                  </div>
+                  <div className="card-body">{ giftcard.title } gift-card can be converted into <br /> { giftcard.currencies.join(", ").toUpperCase() } currencies. </div>
+                </div>
+              )
+            })
+          }
+
+          
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
